b6dd8665c404a21c0f0213b9a49a8be730f38380
ericsaan <ericsaan@outlook.com>
2018-09-01 17:49:28 -0700

    Send notifications by gmail login and devices so can use multiple devices

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+import { exists } from "fs";
+
 //**************************************************************** 
 //Function to send messages to PlaySMS apps for bus status updates
 // Moonspec Design, Copyright 2018
@@ -13,60 +15,79 @@ admin.initializeApp(functions.config().firebase);
 //next figure out how to store tokens and then use them to send messages
 //in future iteration we will actually read this from firestore as we figure out how to keep it 
 //maintatined...ees
-
-const tokenArray = 
-[
-  {"token": "daI69yvIWjM:APA91bEr9laaPA4gVPYuQ-3NBgZB341MiWCId1KfUXYEBlKMQNC4TjRly7-jZ7rtFo9OHGkPQ6UbuHaPoVI1AXhfcF65pzbUbOyXzkpwhbkaVdgA-eF3pftMao9bE6tR_Jr4oIvk6baf", "phoneNumber": "4252411879"},
-  {"token": "daI69yvIWjM:APA91bEr9laaPA4gVPYuQ-3NBgZB341MiWCId1KfUXYEBlKMQNC4TjRly7-jZ7rtFo9OHGkPQ6UbuHaPoVI1AXhfcF65pzbUbOyXzkpwhbkaVdgA-eF3pftMao9bE6tR_Jr4oIvk6baf", "phoneNumber": "4252411902"},
-  {"token": "daI69yvIWjM:APA91bEr9laaPA4gVPYuQ-3NBgZB341MiWCId1KfUXYEBlKMQNC4TjRly7-jZ7rtFo9OHGkPQ6UbuHaPoVI1AXhfcF65pzbUbOyXzkpwhbkaVdgA-eF3pftMao9bE6tR_Jr4oIvk6baf", "phoneNumber": "4257851934"}
-
-];
 const snapshot = data.after;
 const val = snapshot.val();
+
+
+const db = admin.firestore();
+
+
+
+//const tokenArray = queryRef;
+// [
+//   {"token": "fm4b48_BOjg:APA91bFzg3QL58jhHa8rhvpHxZk6q0LwCPxMgbRw8yrbs_yMufyLEtZu5ENlKWWpFXhqmBdV9hRtHu5kvyRzKh1BiJmRkq6icw2Mn4MS8-fM-GP5G3Dr1qYBJGUIt4uUkrlwrP7rJExP", "sender": "ericsaan@gmail.com"},
+//   {"token": "fm4b48_BOjg:APA91bFzg3QL58jhHa8rhvpHxZk6q0LwCPxMgbRw8yrbs_yMufyLEtZu5ENlKWWpFXhqmBdV9hRtHu5kvyRzKh1BiJmRkq6icw2Mn4MS8-fM-GP5G3Dr1qYBJGUIt4uUkrlwrP7rJExP", "sender": "ericsaan@gmail.com"},
+//   {"token": "fm4b48_BOjg:APA91bFzg3QL58jhHa8rhvpHxZk6q0LwCPxMgbRw8yrbs_yMufyLEtZu5ENlKWWpFXhqmBdV9hRtHu5kvyRzKh1BiJmRkq6icw2Mn4MS8-fM-GP5G3Dr1qYBJGUIt4uUkrlwrP7rJExP", "sender": "ericsaan@gmail.com"}
+
+// ];
  
-let tokenOut = "";  
-tokenArray.forEach( function(value) {
-    if (value.phoneNumber === val.Receiver) {
-        tokenOut = value.token;
-        // console.log('got a match!');
-        // console.log('Receiver In');
-        // console.log(val.Receiver);
-        // console.log('TokenArray');
-        // console.log(value.phoneNumber);
-    }
-    
-})
-  
-//now generate the payload for the message
+let tokenOut = "no match";  
 
-const messageOut = {
-    notification: {
-        title: 'Bus Ride Notification- ' + val.Sender,
-        body: String(val.MessageBody)
+var tokenDocs = db.collection('userFcmtokens');
+var queryRef = tokenDocs.where('email', '==',val.Receiver).get()
+.then(snap => {
+    snap.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
         
-    },
-    webpush: {
-        notification: {
-            sound: 'default',
-            badge: '1',
-            icon: 'cartoon-school-bus-clipart-17.png'
+        console.log('Receiver from message db => ',  val.Receiver);
+        console.log('UserDB data.Email is => ', doc.data().email);
+        //console.log('UserDB Email is => ', doc.email());
+
+        if (val.Receiver === doc.data().email) {
+            tokenOut = doc.data().fcmToken;
+            console.log('got a match! ', doc.data().email);
+
+            //now generate the payload for the message
+
+            const messageOut = {
+                notification: {
+                    title: 'Bus Ride Notification- ' + val.Sender,
+                    body: String(val.MessageBody)
+                    
+                },
+                webpush: {
+                    notification: {
+                        sound: 'default',
+                        badge: '1',
+                        icon: 'default'  
+                    }
+                },
+                token: tokenOut
+                };
+
+            console.log(messageOut);
+
+            //dryRun variable to true means the message is validated but not sent.  Good for debugging
+            const dryRun = false;
+
+            admin.messaging().send(messageOut, dryRun)
+            .then((response) => {
+                console.log('Message send successful! ',response);
+            })
+            .catch((error) => {
+                console.log('Error sending message : ',error);
+            });
         }
-    },
-    token: tokenOut
-    };
-
-console.log(messageOut);
-
-//dryRun variable to true means the message is validated but not sent.  Good for debugging
-const dryRun = false;
-
-admin.messaging().send(messageOut, dryRun)
-.then((response) => {
-    console.log('Dry run successful!',response);
+    });
 })
-.catch((error) => {
-    console.log('Error sending message dry run: ',error);
+.catch(err => {
+    console.log('Error getting documents',err);
 });
+    
+
+
+  
+
 
 
 });
